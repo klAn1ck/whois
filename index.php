7fdc64654d44a2008c0c74915254afac2865f444
denis <degordan@googlemail.com>
2013-03-19 19:20:14 +0200

    all

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+$db = mysqli_connect('localhost', 'root', 'ghjcnjnfr21', 'parser_kl');
+
+$p = mysqli_query($db, 'SELECT  * FROM `domains`');
+$r = mysqli_fetch_assoc($p);
+
 //phpinfo();die();
 //echo `whois xado.com`;
 //die();
@@ -9,7 +14,7 @@ $dayAgo = 0;
 while ($dayAgo <= 731) {
     $pageCurrent = 1;
     $timeDayAgo = strtotime('-' . $dayAgo . ' day');
-    $dateLinkText = date('Y-m-d', $timeDayAgo );
+    $dateLinkText = date('Y-m-d', $timeDayAgo);
     $domainsData = array();
 
     do {
@@ -17,15 +22,14 @@ while ($dayAgo <= 731) {
         $postData = http_build_query(
             array(
                 'page' => $pageCurrent,
-                'EXP_DATE' => $dateLinkText,//'2012-10-23',
+                'EXP_DATE' => $dateLinkText, //'2012-10-23',
                 'list_type' => 'PR',
                 'tld' => ''
             )
         );
 
         $opts = array(
-            'http' =>
-            array(
+            'http' => array(
                 'method' => 'POST',
                 'header' => 'Content-type: application/x-www-form-urlencoded',
                 'content' => $postData
@@ -38,25 +42,35 @@ while ($dayAgo <= 731) {
         $content = file_get_contents($mainLink, false, $context);
 
         // parse a links with PR
-        if(preg_match_all('#<tr>\s+<td\s+class=\'ui-widget-content\'><a\s+target="_blank"\s+href=\'/domain-tools/register/([^\']+)\'>.+src="/_images/pr/PageRank(\d+)\.jpg".+</tr>#isU',$content,$domainsDataTemp,PREG_SET_ORDER)){
-            foreach($domainsDataTemp as $key=>$domainDataTemp){
+        if (preg_match_all(
+            '#<tr>\s+<td\s+class=\'ui-widget-content\'><a\s+target="_blank"\s+href=\'/domain-tools/register/([^\']+)\'>.+src="/_images/pr/PageRank(\d+)\.jpg".+</tr>#isU',
+            $content,
+            $domainsDataTemp,
+            PREG_SET_ORDER)) {
+            foreach ($domainsDataTemp as $key => $domainDataTemp) {
                 $domainsData[$pageCurrent][$key]['href'] = $domainDataTemp[1];
                 $domainsData[$pageCurrent][$key]['pr'] = $domainDataTemp[2];
 
-                $whoisData = shell_exec('whois '.$domainDataTemp[1]);
-                $whoIsExpiredStatus = preg_match('#Expiration\s+Date\:\s+(\d{2}-\S{3}-\d{4})#is',$whoisData,$whoistemp) ? strtotime($whoistemp[1]) : 0;
-                $expiredStatusFlag =  $timeDayAgo >= $whoIsExpiredStatus ? 1 : 0 ;
+                $whoisData = shell_exec('whois ' . $domainDataTemp[1]);
+                $whoisExpiredStatus = preg_match(
+                    '#Expiration\s+Date\:\s+(\d{2}-\S{3}-\d{4})#is',
+                    $whoisData,
+                    $whoistemp
+                ) ? strtotime($whoistemp[1]) : 0;
+                $expiredStatusFlag = $timeDayAgo >= $whoisExpiredStatus ? 1 : 0;
 
-                echo $whoIsExpiredStatus;
+                echo $whoisExpiredStatus;
                 die();
+                $db
             }
         }
 
         # get the count of pages
-        $pageCount = !isset($pageCount) ? (preg_match('#<span\s+class="pages">[^<]+<b>\d+</b>[^<]+<b>(\d+)</b></span>#is',
-                                            $content,
-                                            $pageArr
-                                        ) ? $pageArr['1'] : 1) : $pageCount;
+        $pageCount = !isset($pageCount) ? (preg_match(
+            '#<span\s+class="pages">[^<]+<b>\d+</b>[^<]+<b>(\d+)</b></span>#is',
+            $content,
+            $pageArr
+        ) ? $pageArr[1] : 1) : $pageCount;
         ++$pageCurrent;
     } while ($pageCurrent <= $pageCount);
 
