47a1af6b13a12f4b4b14f8e29848e0647e950631
klAn1ck <degordan@googlemail.com>
2013-03-20 09:51:30 +0200

    temp

--- index.php
+++ index.php
@@ -1,45 +1,90 @@
 <?php
-$timeNow = strtotime('now');
+$db = mysqli_connect('localhost', 'root', 'ghjcnjnfr21', 'parser_kl');
+
+function getDataByLink($link){
+    $content = file_get_contents('http://ahrefs.com/site-explorer/backlinks/subdomains/'.$link.'/not_sitewide-href-dofollow-all-all');
+    $indexAll = preg_match('#id="filter-all-gov-edu-counter">(\d+)</span>#isU',$content,$tempAll) ? $tempAll[1] : 0 ;
+    $indexGov = preg_match('#id="filter-gov-counter">(\d+)</span>#isU',$content,$tempGov) ? $tempGov[1] : 0 ;
+    $indexEdu = preg_match('#id="filter-edu-counter">(\d+)</span>#isU',$content,$tempEdu) ? $tempEdu[1] : 0 ;
+
+    return array($indexAll,$indexGov,$indexEdu);
+}
+
+
+//$p = mysqli_query($db, 'SELECT  * FROM `domains`');
+//$r = mysqli_fetch_assoc($p);
+
+//phpinfo();die();
+//echo `whois xado.com`;
+//die();
+//$timeNow = strtotime('now');
 
 $dayAgo = 0;
 while ($dayAgo <= 731) {
     $pageCurrent = 1;
+    $timeDayAgo = strtotime('-' . $dayAgo . ' day');
+    $dateLinkText = date('Y-m-d', $timeDayAgo);
+    $domainsData = array();
 
     do {
-        $dateLinkText = date('Y-m-d', strtotime('-' . $dayAgo . ' day'));
+        // prepare POST for file_get_contents function
         $postData = http_build_query(
             array(
                 'page' => $pageCurrent,
-                'EXP_DATE' => $dateLinkText,//'2012-10-23',
+                'EXP_DATE' => $dateLinkText, //'2012-10-23',
                 'list_type' => 'PR',
                 'tld' => ''
             )
         );
 
         $opts = array(
-            'http' =>
-            array(
+            'http' => array(
                 'method' => 'POST',
                 'header' => 'Content-type: application/x-www-form-urlencoded',
                 'content' => $postData
             )
         );
 
+        // get the content of the current page
         $mainLink = 'http://www.odditysoftware.com/__live/_post/domains.php';
         $context = stream_context_create($opts);
         $content = file_get_contents($mainLink, false, $context);
-        if($pageCurrent === 2) die($content);
 
-        $pageCount = isset($pageCount) ? (preg_match('#<span\s+class="pages">[^<]<b>\d+</b>[^<]<b>(\d+)</b></span>#is',
-                                            $content,
-                                            $pageArr
-                                        ) ? $pageArr['1'] : 1) : $pageCount;
-        die($pageCount);
+        // parse a links with PR
+        if (preg_match_all(
+            '#<tr>\s+<td\s+class=\'ui-widget-content\'><a\s+target="_blank"\s+href=\'/domain-tools/register/([^\']+)\'>.+src="/_images/pr/PageRank(\d+)\.jpg".+</tr>#isU',
+            $content,
+            $domainsDataTemp,
+            PREG_SET_ORDER)) {
+            foreach ($domainsDataTemp as $key => $domainDataTemp) {
+                $domainsData[$pageCurrent][$key]['href'] = $domainDataTemp[1];
+                $domainsData[$pageCurrent][$key]['pr'] = $domainDataTemp[2];
+                $domainsData[$pageCurrent][$key]['pr'] = getDataByLink($domainsData[$pageCurrent][$key]['href']);
+
+                $whoisData = shell_exec('whois ' . $domainDataTemp[1]);
+                $whoisExpiredStatus = preg_match(
+                    '#Expiration\s+Date\:\s+(\d{2}-\S{3}-\d{4})#is',
+                    $whoisData,
+                    $whoistemp
+                ) ? strtotime($whoistemp[1]) : 0;
+                $expiredStatusFlag = $timeDayAgo >= $whoisExpiredStatus ? 1 : 0;
+
+                echo $whoisExpiredStatus;
+                die();
+
+            }
+        }
+
+        # get the count of pages
+        $pageCount = !isset($pageCount) ? (preg_match(
+            '#<span\s+class="pages">[^<]+<b>\d+</b>[^<]+<b>(\d+)</b></span>#is',
+            $content,
+            $pageArr
+        ) ? $pageArr[1] : 1) : $pageCount;
         ++$pageCurrent;
     } while ($pageCurrent <= $pageCount);
 
 
-
-    die($content);
+    die();
     $dayAgo++;
 }
